f17f201579865151c21f95cc29ff36cdc269dbee
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-03-16 14:52:53 +0500

    UPDATE: :construction: basic serviceWorker setup

---
+++ public/serviceWorker.js
@@ -0,0 +1,47 @@
+var cacheName = "pwa-practice-v3";
+var filesToCache = [
+  "/",
+  "/index.html",
+  "/assets/img/1.jpg",
+  "/assets/img/2.jpg",
+  "/assets/img/3.jpg",
+  "/assets/js/main.js"
+];
+
+self.addEventListener("install", function(e) {
+  console.log("[ServiceWorker] Install");
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log("[ServiceWorker] Caching app shell");
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", function(e) {
+  console.log("[ServiceWorker] Activate");
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(
+        keyList.map(function(key) {
+          console.log("Keys", key);
+
+          if (key !== cacheName) {
+            console.log("[ServiceWorker] Removing old cache", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener("fetch", function(e) {
+  console.log("[Service Worker] Fetch", e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
